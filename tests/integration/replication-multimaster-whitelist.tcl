35687ef7e9a66837be7bb7d35ea4da7f96325844
atharvagore <atharvammg@gmail.com>
2026-07-14 21:55:16 +0530

    made changes to the tcl test file
    Signed-off-by: atharvagore <atharvammg@gmail.com>

--- tests/integration/replication-multimaster-whitelist.tcl
+++ tests/integration/replication-multimaster-whitelist.tcl
@@ -22,9 +22,10 @@ start_server {tags {"repl external:skip"}} {
             set dbid 0
             regexp {db=([0-9]+)} [$node0 client info] _ dbid
 
-            $node1 replicaof $node0_host $node0_port
+            $node1 multimaster add $node0_host $node0_port
             wait_for_condition 100 100 {
-                [s 0 master_link_status] eq {up}
+                [s -1 active_upstream_runtime_links] >= 1 &&
+                [s 0 active_upstream_runtime_links] >= 1
             } else {
                 fail "Initial replica link not established"
             }
@@ -32,16 +33,16 @@ start_server {tags {"repl external:skip"}} {
 
         test {Non-whitelisted write is rejected and not applied locally} {
             $node0 del mm:wl:reject
-            $node0 config set crdt-whitelist "lset"
-            assert_error "*not CRDT-whitelisted*" {$node0 rpush mm:wl:reject a}
+            $node0 config set multi-master-whitelist "lset"
+            assert_error "ERR command 'rpush' is not whitelisted in multi-master mode. The operation was not applied." {$node0 rpush mm:wl:reject a}
             # The command must not have touched the local keyspace.
             assert_equal 0 [$node0 exists mm:wl:reject]
-            $node0 config set crdt-whitelist ""
+            $node0 config set multi-master-whitelist ""
         }
 
         test {Whitelisted command executes locally and replicates via RREPLAY} {
             $node0 del mm:wl:list
-            $node0 config set crdt-whitelist "rpush lset"
+            $node0 config set multi-master-whitelist "rpush lset"
             assert_equal 3 [$node0 rpush mm:wl:list a b c]
             assert_equal OK [$node0 lset mm:wl:list 0 X]
             assert_equal {X b c} [$node0 lrange mm:wl:list 0 -1]
@@ -50,19 +51,19 @@ start_server {tags {"repl external:skip"}} {
             } else {
                 fail "whitelisted write did not replicate to node1"
             }
-            $node0 config set crdt-whitelist ""
+            $node0 config set multi-master-whitelist ""
         }
 
-        test {CONFIG SET/GET crdt-whitelist works at runtime} {
-            $node0 config set crdt-whitelist "lset set hset"
-            set got [lindex [$node0 config get crdt-whitelist] 1]
+        test {CONFIG SET/GET multi-master-whitelist works at runtime} {
+            $node0 config set multi-master-whitelist "lset set hset"
+            set got [lindex [$node0 config get multi-master-whitelist] 1]
             assert_equal {hset lset set} [lsort $got]
-            $node0 config set crdt-whitelist ""
-            assert_equal {} [lindex [$node0 config get crdt-whitelist] 1]
+            $node0 config set multi-master-whitelist ""
+            assert_equal {} [lindex [$node0 config get multi-master-whitelist] 1]
         }
 
         test {Empty whitelist allows all commands (backward compat)} {
-            $node0 config set crdt-whitelist ""
+            $node0 config set multi-master-whitelist ""
             $node0 del mm:wl:empty
             assert_equal 3 [$node0 rpush mm:wl:empty a b c]
             assert_equal 3 [$node0 llen mm:wl:empty]
@@ -70,26 +71,24 @@ start_server {tags {"repl external:skip"}} {
 
         test {CRDT metadata none round-trips through RREPLAY} {
             $node0 del mm:meta:ok
-            $node0 config set crdt-whitelist "set"
+            $node0 config set multi-master-whitelist "set"
             assert_equal OK [$node0 replconf capa rreplay-peer]
-            assert_equal OK [$node0 replconf uuid 4444444444444444444444444444444444444444]
             set ts [expr {[s -1 hlc_clock_wall] + 100000}]
             assert_equal 30001 [$node0 rreplay 5555555555555555555555555555555555555555 $dbid 30001 $ts-0 none set mm:meta:ok yes]
             assert_equal "yes" [$node0 get mm:meta:ok]
-            $node0 config set crdt-whitelist ""
+            $node0 config set multi-master-whitelist ""
         }
 
         test {Non-none CRDT metadata is rejected gracefully} {
             $node0 del mm:meta:bad
-            $node0 config set crdt-whitelist "set"
+            $node0 config set multi-master-whitelist "set"
             assert_equal OK [$node0 replconf capa rreplay-peer]
-            assert_equal OK [$node0 replconf uuid 4444444444444444444444444444444444444444]
             set ts [expr {[s -1 hlc_clock_wall] + 100000}]
             # Unrecognized metadata is skipped without tearing down the link: the
             # peer link still ACKs with the replay id, but the command is dropped.
             assert_equal 30002 [$node0 rreplay 5555555555555555555555555555555555555555 $dbid 30002 $ts-0 rga:anchor set mm:meta:bad nope]
             assert_equal 0 [$node0 exists mm:meta:bad]
-            $node0 config set crdt-whitelist ""
+            $node0 config set multi-master-whitelist ""
         }
     }
 }
